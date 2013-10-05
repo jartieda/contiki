c5128ae4bfec870dc387f9b2d814e05f0277e214
unknown <jartieda@yahoo.com>
2013-10-05 19:18:07 +0200

    wind sensor not working yet

---
+++ platform/stm32f4discovery/windsensor.h
@@ -0,0 +1,24 @@
+/*
+ * windsensor.h
+ *
+ *  Created on: 5 Oct 2013
+ *      Author: Jorge
+ */
+
+#ifndef WINDSENSOR_H_
+#define WINDSENSOR_H_
+#include "stm32f4xx_conf.h"
+
+#include "lib/sensors.h"
+
+extern const struct sensors_sensor wind_sensor;
+
+#define WIND_SENSOR "WIND"
+
+#define WIND_SPEED 1
+#define WIND_DIR 2
+
+
+
+
+#endif /* WINDSENSOR_H_ */
